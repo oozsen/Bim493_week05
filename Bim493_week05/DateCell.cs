79b2cca0ee7fac65ed0e02b8fb7662f70adc0a03
oozsen <604e2d6124958f4a79563d8f6e8873510690c094>
2020-11-02 14:21:08 +0300

    * DateCell.cs: DateCell class ı içerisinde bindable Property olarak   LabelProperty eklendi.
    * DateCell.xaml: Xaml tarafta da Label Property olarak eklendi.
    
    * MyPage4.xaml:

---
+++ Bim493_week05/DateCell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Bim493_week05
+{
+    public partial class DateCell : ViewCell
+    {
+        public DateCell()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create(
+                "Label",
+                typeof(string),
+                typeof(DateCell));
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+     }
+}
